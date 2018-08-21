9d96975ad9a819cca2a401af00edc223355579a7
yuvraj <yuvraj_2300@hotmail.com>
2018-08-21 23:34:50 +0530

    update aop

--- DesignPatterns/src/main/java/com/dp/DesignPatterns/AOP/ee/SecurityInterceptor.java
+++ DesignPatterns/src/main/java/com/dp/DesignPatterns/AOP/ee/SecurityInterceptor.java
@@ -2,6 +2,8 @@ package com.dp.DesignPatterns.AOP.ee;
 
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
@@ -14,7 +16,26 @@ public class SecurityInterceptor {
 	public	Object	doSecurityCheck(InvocationContext	context) throws Exception {
 		//do logic
 		Logger.getLogger("SecurityLog").info(context.getMethod().getName()+" is accessed");
-		return	context.proceed();
 		
+		//Verifyinng user's existennce
+		String	user	=	(String) context.getContextData().get("user");
+		
+		if(user==null) {
+			user	=	(String)context.getParameters()[0];
+			context.getContextData().put("user", user);
+		}
+		
+		return	context.proceed();
 	}
+	
+	@PostConstruct
+	public	void	onStart() {
+		Logger.getLogger("SecurityLog").info("Activitating");
+	}
+	
+	@PreDestroy
+	public	void	onStop() {
+		Logger.getLogger("SecurityLog").info("Deactivating");
+	}
+	
 }
